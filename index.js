a86fcbabce4e3d2b5a160d1db0afee0e670ed479
Valeriy Martsenyuk <vmartsen@contractor.amplify.com>
2014-02-17 17:27:40 +0200

    XX

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var http = require('http');
 http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('GIT HUB hello world');
+    res.end('GIT HUB hello world !!! just by commit auto deploy');
 }).listen(8080);
